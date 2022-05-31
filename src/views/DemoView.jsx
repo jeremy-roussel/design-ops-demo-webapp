be120c8841ad6fc6c143501bcb6380f35ac73eb2
Jeremy Roussel <jeremy.roussel@umbrage.com>
2022-05-31 10:06:38 -0500

    Initial Commit

---
+++ src/views/DemoView.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Demo } from '@jeremyr/design-ops-library';
+
+export const DemoView = () => (
+  <div>
+    <h1>This is your brand new application scaffold!</h1>
+    <Demo />
+  </div>
+);
