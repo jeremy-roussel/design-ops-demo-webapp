2d43e9c52a902bc1e84aa0cd1e079271ff6c8427
Jeremy Roussel <jeremy.roussel@umbrage.com>
2022-05-31 10:10:53 -0500

    Remove local-only component

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-import { Demo } from '@jeremyr/design-ops-library';
-
 export const DemoView = () => (
   <div>
     <h1>This is your brand new application scaffold!</h1>
-    <Demo />
   </div>
 );
